a0d60985ab88604287d9e42c5305449eb404f47b
Adrian Wadey <adrian.j.wadey@gmail.com>
2018-02-10 14:00:18 +0000

    removed test junk from delete.php

--- delete.php
+++ delete.php
@@ -7,17 +7,5 @@ $db = new Db();
 $response = $db->mark_complete_by_id($_GET['id']);
 
 //go back to the main page
-//header("location: index.php");
+header("location: index.php");
 ?>
-<html>
-    <body>
-    <p>You will be redirected in 3 seconds</p>
-    <script>
-        var timer = setTimeout(function() {
-            window.location='index.php'
-        }, 3000);
-    </script>
-</body>
-</html>
-
-
